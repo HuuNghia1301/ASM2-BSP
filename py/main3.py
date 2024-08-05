4c931dfddb6fd3c7fc9344948518106a025a0394
huunghia <140091966+bannghiagiauten@users.noreply.github.com>
2024-08-05 17:07:05 +0700

    Delete py directory

--- py/main3.py
+++
@@ -1,9 +0,0 @@
-num1 = int(input('Nhap so thu nhat:'))
-num2 = int(input('Nhap so thu hai:'))
-num3 = int(input('Nhap so thu ba:'))
-
-max_x = max(num1,num2,num3)
-print(max_x)
-min_x = min(num1,num2,num3)
-print(min_x)
-
--- py/num.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-import numpy as np 
-
-ten_tap_tin = 'customer.csv.txt'
-with open(ten_tap_tin, 'r', encoding='utf-8') as file:
-    test = file.read()
-    print(test)
