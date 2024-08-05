91e2bda060a9bf339aa2a2f5653cb21e2b1e103a
huunghia <140091966+bannghiagiauten@users.noreply.github.com>
2024-08-05 15:44:11 +0700

    Add files via upload

---
+++ py/main3.py
@@ -0,0 +1,9 @@
+num1 = int(input('Nhap so thu nhat:'))
+num2 = int(input('Nhap so thu hai:'))
+num3 = int(input('Nhap so thu ba:'))
+
+max_x = max(num1,num2,num3)
+print(max_x)
+min_x = min(num1,num2,num3)
+print(min_x)
+
